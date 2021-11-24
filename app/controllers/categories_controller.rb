d49a78fd0100699240cb59a0fba534b76f802905
Arianne Canlubo <amcanlubo@gmail.com>
2021-11-24 14:17:40 +0800

    Added delete in controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -40,6 +40,12 @@ class CategoriesController < ApplicationController
     end 
   end
 
+  def delete
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
   
   private
    
